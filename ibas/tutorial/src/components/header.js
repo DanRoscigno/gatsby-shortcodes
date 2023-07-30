0ed0cf84386ad1723fe324150efbf4237f63f966
DanRoscigno <dan@roscigno.com>
2023-07-30 00:07:31 -0400

    underline active page link

--- ibas/tutorial/src/components/header.js
+++ ibas/tutorial/src/components/header.js
@@ -14,20 +14,30 @@ const Header = () => {
       </p>
     </div>
     <nav className={headerStyles.navContainer}>
-      <ul className={headerStyles.navList}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blog/">Blog</Link>
-          </li>
-          <li>
-            <Link to="/contact/">Contact</Link>
-          </li>
-          <li>
-            <Link to="/about/">About</Link>
-          </li>
-        </ul>
+    <ul className={headerStyles.navList}>
+      <li>
+        <Link to="/" activeClassName={headerStyles.activeMenuItem}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/blog/" activeClassName={headerStyles.activeMenuItem}>
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/contact/"
+            activeClassName={headerStyles.activeMenuItem}>
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link to="/about/" activeClassName={headerStyles.activeMenuItem}>
+            About
+          </Link>
+        </li>
+      </ul>
     </nav>
   </header>
 );
